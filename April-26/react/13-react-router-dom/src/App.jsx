440e1bb3b81421efa8433a6ba75eb85f89a475de
codekeyboard <saim.codecraft@gmail.com>
2026-04-16 14:00:32 +0500

    react touter dom browser router nested router and dynamic router

---
+++ April-26/react/13-react-router-dom/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
+export default App
